00c470bc2d7074935045bb5902621c1ff1d22364
bmavity <brian@brianmavity.com>
2014-09-04 08:50:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+ges-test-helper
+=======
+
+**An In-Memory Test Helper for [(Get) Event Store](http://geteventstore.com)**
+
+
+<a name="license"></a>
+License &amp; copyright
+-------------------
+
+Copyright (c) 2014 Brian Mavity.
+
+ges-test-helper is licensed under the MIT license. All rights not explicitly granted in the MIT license are reserved. See the included LICENSE.md file for more details.
+
+
+Event Store License
+
+Please see https://github.com/EventStore/EventStore
+
---
+++ ges-test-helper.js
@@ -0,0 +1,50 @@
+var spawn = require('child_process').spawn
+	, path = require('path')
+	, cmdDir = path.resolve(__dirname, 'EventStore')
+	, cmd = path.resolve(cmdDir, 'clusternode')
+	, opts = {
+			cwd: cmdDir
+		, env: {
+				'LD_LIBRARY_PATH': cmdDir + ':$LD_LIBRARY_PATH'
+			, 'MONO_GC_DEBUG': 'clear-at-gc'
+			}
+		}
+
+module.exports = function createMemoryEventStore(args, cb) {
+	if(!cb && typeof args === 'function') {
+		cb = args
+		args = {}
+	}
+
+	var es = spawn(cmd, getArgsArray(args), opts)
+
+	es.stdout.on('data', function(data) {
+		console.log(data.toString())
+		if(data.toString().indexOf('Starting Normal TCP listening on TCP endpoint:') !== -1) {
+			cb(null, es)
+		}
+	})
+
+	es.stderr.on('data', function(data) {
+		console.log(data.toString())
+		cb(data.toString())
+	})
+
+/*
+	es.on('close', function(err, signal) {
+		if(err) return console.log('ES process closed with error: ', err)
+		console.log('ES process closed with sig: ' + signal)
+	})
+*/
+
+	es.on('error', function(err) {
+		cb(err)
+	})
+}
+
+function getArgsArray(args) {
+	var allArgs = ['--mem-db']
+	if(args.ip) allArgs.push('--ip=' + args.ip) 
+	if(args.tcpPort) allArgs.push('--tcp-port=' + args.tcpPort) 
+	return allArgs
+}
